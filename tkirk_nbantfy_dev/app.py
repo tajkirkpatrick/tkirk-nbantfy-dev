fd29c725f386e2465b8b59c3a44a30c7188091f0
tajkirkpatrick <50558516+tajkirkpatrick@users.noreply.github.com>
2023-11-01 17:59:27 -0500

    isolate the table body

--- tkirk_nbantfy_dev/app.py
+++ tkirk_nbantfy_dev/app.py
@@ -1,16 +1,23 @@
 import urllib3
 from bs4 import BeautifulSoup
 
+
 def main():
     # Creating a PoolManager instance for sending requests.
     http = urllib3.PoolManager()
 
     # Sending a GET request and getting back response as HTTPResponse object.
-    resp = http.request("GET", "https://www.basketball-reference.com/teams/HOU/2024_games.html")
+    resp = http.request(
+        "GET", "https://www.basketball-reference.com/teams/HOU/2024_games.html#games"
+    )
 
-    soup = BeautifulSoup(resp.data, 'html.parser')
+    soup = BeautifulSoup(resp.data, "html.parser")
 
-    print(soup.prettify())
+    schedule_table = soup.find_all("table", id="games")[0]
+    table_body = schedule_table.find("tbody")
+
+    print(table_body)
+
 
 if __name__ == "__main__":
     main()
